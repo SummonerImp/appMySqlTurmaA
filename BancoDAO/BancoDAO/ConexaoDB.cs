d39bb4382a32586ad184a814163aae24e53efd6f
SummonerImp <csantanajunior2012@gmail.com>
2020-11-01 17:07:14 -0300

    initial commit

---
+++ BancoDAO/BancoDAO/ConexaoDB.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace BancoDAO
+{
+    class ConexaoDB
+    {
+        private static ConexaoDB objConexaoDB = null;
+        private readonly MySqlConnection conexao;
+        private ConexaoDB()
+        {
+            conexao = new MySqlConnection("server=localhost;user id=root;password=1234567;persistsecurityinfo=True;database=dbturmaa");
+        }
+
+        public static ConexaoDB ConexaoStatus()
+        {
+            if (objConexaoDB == null)
+            {
+                objConexaoDB = new ConexaoDB();
+            }
+            return objConexaoDB;
+        }
+
+        public MySqlConnection ConexaoPegar()
+        {
+            return conexao;
+        }
+    }
+}
---
+++ query.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS dbturmaa;
+CREATE DATABASE dbTurmaA;
+use dbturmaa;
